7521a9e16d990c342e79324d646ed375f33f93fe
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-06 13:16:43 +0000

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import React from 'react'
 import Clickme from './components/Clickme';
 import Chartme from './components/Chartme';
+import AutoComplete from './components/AutoComplete';
 
 export default function App() {
   return (
     <>
         <Clickme/>
+        <br/>
         <Chartme/>
+        <br/>
+        <AutoComplete/>
     </>
   )
 }
